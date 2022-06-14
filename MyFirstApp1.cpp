26b6e196a0f8b5cad06f4ba9217319990e41068b
obodoekwe <88957390+obodoekwe@users.noreply.github.com>
2022-06-14 16:47:51 -0700

    Add files via upload

---
+++ MyFirstApp1.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n";// this will output hello world in the console
+	cout << "My name is Chijioke." << endl;
+	cout << "Today is Wednesday\n";
+	cout << "Have a nice day";
+
+	system("pause>0");
+}
+
